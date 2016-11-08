759971e0f44d8bf11c58ff7d16789c441c20afcd
Dhirendra Kumar Tiwari <dhirendra.tiwari@tavant.com>
2016-11-08 10:07:57 +0530

    Code cleaning

--- src/main/java/com/liveStream/LoadProperty.java
+++ src/main/java/com/liveStream/LoadProperty.java
@@ -1,67 +1,52 @@
 package com.liveStream;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 public class LoadProperty {
 
-	
-	Properties prop =null;
-	
+	private final static Logger LOGGER = Logger.getLogger(LoadProperty.class
+			.getName());
+
+	Properties prop = null;
+
 	public LoadProperty() {
-		// TODO Auto-generated constructor stub
-	if(prop==null)
-		getPropertiesObj();
-	
-	
+		if (prop == null)
+			getPropertiesObj();
 	}
-	
-	
-	public String getProperty(String key)
-	{
-		String value="";
-		
-		value=prop.getProperty(key);
-		
+
+	public String getProperty(String key) {
+		String value = "";
+		value = prop.getProperty(key);
 		return value;
-		
-		
 	}
-	
-	public  Properties getPropertiesObj()
-	{
-		
+
+	public Properties getPropertiesObj() {
+
 		InputStream input = null;
-
 		String key = "ABC";
-		String value="";
+		String value = "";
 
-			String filename = "config.properties";
-			input = getClass().getClassLoader().getResourceAsStream(filename);
-			if (input == null) {
-				System.out.println("Sorry, unable to find " + filename);
-				
-			}else{
-				
+		String filename = "config.properties";
+		input = getClass().getClassLoader().getResourceAsStream(filename);
+		if (input == null) {
+			LOGGER.info("Sorry, unable to find " + filename);
+
+		} else {
 			try {
-				prop=new Properties();
+				prop = new Properties();
 				prop.load(input);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			} catch (Exception e) {
+				LOGGER.error(e);
 			}
+			value = prop.getProperty(key);
+			LOGGER.info("Key : " + key + ", Value : " + value);
 
-			
-				value = prop.getProperty(key);
-				System.out.println("Key : " + key + ", Value : " + value);
-		
-			}
-			
-			
-				return prop;
+		}
+
+		return prop;
 	}
-	
-	
-	
+
 }
